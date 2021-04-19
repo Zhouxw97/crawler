93e92d0bb49fa8d8e90475f9507472fee51e58c2
zhouxiaowen <00441208@want-want.com>
2021-04-19 10:56:20 +0800

    添加测试类

--- src/main/java/cn/guyu/service/ItemService.java
+++ src/main/java/cn/guyu/service/ItemService.java
@@ -5,16 +5,24 @@ import cn.guyu.pojo.Item;
 import java.util.List;
 
 public interface ItemService {
+
     /**
      * 保存商品
+     *
      * @param item
+     * @return: void
+     * @author: zhouxiaowen
+     * @date: 2021-04-19 10:21
      */
     public void save(Item item);
 
     /**
      * 根据条件查询物品
+     *
      * @param item
-     * @return
+     * @return: java.util.List<cn.guyu.pojo.Item>
+     * @author: zhouxiaowen
+     * @date: 2021-04-19 10:21
      */
     public List<Item> findAll(Item item);
 }
