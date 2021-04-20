ebb1b46ed0f0d7147154d3a782500140efb8a5ea
zhouxiaowen <00441208@want-want.com>
2021-04-20 19:15:13 +0800

    修改页面解析

--- src/main/java/com/wantwant/task/ItemTask.java
+++ src/main/java/com/wantwant/task/ItemTask.java
@@ -1,11 +1,15 @@
 package com.wantwant.task;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.wantwant.entity.po.ItemPo;
 import com.wantwant.enums.StatusEnum;
-import com.wantwant.pojo.Item;
 import com.wantwant.service.ItemService;
 import com.wantwant.utils.HttpUtils;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -13,8 +17,10 @@ import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 定时爬取数据
@@ -24,25 +30,28 @@ import java.util.List;
  * @version 1.0
  */
 @Component
+@Slf4j
 public class ItemTask {
+
     @Autowired
     private HttpUtils httpUtils;
+
     @Autowired
     private ItemService itemService;
-    //用于解析json
-    @Autowired
-    private ObjectMapper MAPPER ;
 
     //当下载任务完成后，间隔多长时间进行下次任务。
     //@Scheduled(fixedDelay = 100 * 1000)
+    @SneakyThrows
     public void itemTask() throws JsonProcessingException {
         //1.声明地址
-        String url = "https://search.jd.com/Search?keyword=%E6%97%BA%E6%97%BA&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&suggest=1.def.0.V18--12s0%2C20s0%2C38s0%2C97s0&wq=shouji&s=122&click=0&page=";
+        String url = "https://search.jd.com/Search?keyword=%E6%97%BA%E6%97%BA&qrst=1&wq=%E6%97%BA%E6%97%BA&stock=1&ev=exbrand_%E6%97%BA%E6%97%BA%5E&click=0&page=";
         //2.遍历页面
-        for (int i = 1; i < 5; i = i + 2) {
-            String html = httpUtils.doGetHtml(url + i);
+        for (int i = 1; i < 2; i = i + 2) {
+            log.info("url:{}",url+i);
+            String html = httpUtils.doGetHtml(url + i,false);
             //解析页面，获取商品数据并存储
             parse(html);
+            Thread.sleep(200l);
         }
     }
 
@@ -68,33 +77,120 @@ public class ItemTask {
                 spu = Long.parseLong(spuStr);
                 Long sku = Long.parseLong(element.attr("data-sku"));//sku
                 //根据sku查询数据库这个商品,有的话跳过
-                Item item = new Item();
-                item.setSpu(sku);
-                List<Item> itemList = itemService.findAll(item);
+                Wrapper<ItemPo> lambdaQueryWrapper = new LambdaQueryWrapper<ItemPo>().eq(ItemPo::getSpu, sku);
+                List<ItemPo> itemList = itemService.list(lambdaQueryWrapper);
                 if (itemList.size()>0){
                     continue;
                 }
 
+
+                //列表标题
+                String title = element.select("div.p-name > a > em").text();
+                //价格
+                String price = element.select("div.p-price > strong > i").text();
+                //店铺
+                String store = element.select("div.p-shop > span > a").text();
                 //商品的详情url
                 String itemUrl= "https://item.jd.com/"+sku+".html";
                 String imgUrl="https:"+element.select("img").attr("data-lazy-img");
-                //价格
-                String price = element.select("div.p-price > strong > i").text();
                 //标题
-                String itemInfo = httpUtils.doGetHtml(itemUrl);
-                String title = Jsoup.parse(itemInfo).select("div.sku-name").text();
-                System.out.println("title = " + title);
-                item.setTitle(title);
-                item.setSpu(spu);//设置商品spu
+                String itemInfo = httpUtils.doGetHtml(itemUrl,true);
+                if (StringUtils.isEmpty(title)) {
+                    title = Jsoup.parse(itemInfo).select("div#itemName").text();
+                }
+                //促销活动
+                Elements script = Jsoup.parse(itemInfo).getElementsByTag("script");
+                String scriptStr = script.toString();
+                StringBuilder stringBuilder = new StringBuilder();
+
+                //获取19促销内容
+                String patternStr19 = "\"19\":(.*?)(\",)";
+                Pattern pattern19 = Pattern.compile(patternStr19);
+                Matcher matcher19 = pattern19.matcher(scriptStr);
+                if(matcher19.find()){
+                    String promotions = matcher19.group().trim();
+                    if (!StringUtils.isEmpty(promotions)){
+                        stringBuilder.append(promotions.substring(6, promotions.length() - 2));
+                        stringBuilder.append(";");
+                    }
+                }
+
+                //获取60促销内容
+                String patternStr60 = "\"60\":(.*?)(\",)";
+                Pattern pattern60 = Pattern.compile(patternStr60);
+                Matcher matcher60 = pattern60.matcher(scriptStr);
+                if(matcher60.find()){
+                    String promotions = matcher60.group().trim();
+                    if (!StringUtils.isEmpty(promotions)){
+                        stringBuilder.append(promotions.substring(6, promotions.length() - 2));
+                        stringBuilder.append(";");
+                    }
+                }
+
+                ItemPo item = new ItemPo();
+                item.setSpu(spu);
                 item.setSku(sku);
+                item.setTitle(title);
+                item.setStore(store);
+                item.setPromotions(stringBuilder.toString());
                 item.setUrl(itemUrl);
-                item.setPic(imgUrl);//商品图片
-                item.setPrice(Double.valueOf(price));//价格
-                item.setCreated(new Date());
-                item.setUpdated(new Date());
+                item.setPic(imgUrl);
+                item.setPrice(Double.valueOf(price));
+                item.setCreated(LocalDateTime.now());
                 itemService.save(item);
             }
         }
+    }
+
+    /**
+     * 获取商品详情页信息
+     *
+     * @param
+     * @return: void
+     * @author: zhouxiaowen
+     * @date: 2021-04-20 19:11
+     */
+    public void getItemDetails(){
+        String itemUrl= "https://item.jd.com/1152038.html";
+        //标题
+        String itemInfo = httpUtils.doGetHtml(itemUrl,false);
+        String title = Jsoup.parse(itemInfo).select("div.sku-name").text();
+        log.info("title:{}",title);
+        /*String itemInfo = httpUtils.doGetHtml(itemUrl,true);
+        String title = Jsoup.parse(itemInfo).select("div#itemName").text();
+        log.info("title:{}",title);*/
+
+        //促销活动
+        Elements script = Jsoup.parse(itemInfo).getElementsByTag("script");
+        String scriptStr = script.toString();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        //获取19促销内容
+        String patternStr19 = "\"19\":(.*?)(\",)";
+        Pattern pattern19 = Pattern.compile(patternStr19);
+        Matcher matcher19 = pattern19.matcher(scriptStr);
+        if(matcher19.find()){
+            String promotions = matcher19.group().trim();
+            if (!StringUtils.isEmpty(promotions)){
+                stringBuilder.append(promotions.substring(6, promotions.length() - 2));
+                stringBuilder.append(";");
+            }
+        }
+
+        //获取60促销内容
+        String patternStr60 = "\"60\":(.*?)(\",)";
+        Pattern pattern60 = Pattern.compile(patternStr60);
+        Matcher matcher60 = pattern60.matcher(scriptStr);
+        if(matcher60.find()){
+            String promotions = matcher60.group().trim();
+            if (!StringUtils.isEmpty(promotions)){
+                stringBuilder.append(promotions.substring(6, promotions.length() - 2));
+                stringBuilder.append(";");
+            }
+        }
+
+        log.info("stringBuilder:{}",stringBuilder.toString());
+
     }
 
 }
