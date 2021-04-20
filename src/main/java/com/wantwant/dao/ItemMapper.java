61b9b426af09b3a5cf1209e474808e70488808cb
zhouxiaowen <00441208@want-want.com>
2021-04-20 14:12:15 +0800

    dao

---
+++ src/main/java/com/wantwant/dao/ItemMapper.java
@@ -0,0 +1,16 @@
+package com.wantwant.dao;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wantwant.entity.po.ItemPo;
+
+/**
+ * Mapper 接口
+ *
+ * @author zhouxiaowen
+ * @date 2021-04-20 10:49
+ * @version 1.0
+ */
+public interface ItemMapper extends BaseMapper<ItemPo>{
+
+}
