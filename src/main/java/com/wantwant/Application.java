2973958000efa78709d1c3c395a8616bdd933212
zhouxiaowen <00441208@want-want.com>
2021-04-20 14:09:38 +0800

    启动类修改

--- src/main/java/com/wantwant/Application.java
+++ src/main/java/com/wantwant/Application.java
@@ -2,7 +2,10 @@ package com.wantwant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * 启动类
@@ -13,6 +16,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication
 @EnableScheduling  //开启定时任务的注解
+@EnableTransactionManagement
+@EnableSwagger2
+@ComponentScan(basePackages = "com.wantwant")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
